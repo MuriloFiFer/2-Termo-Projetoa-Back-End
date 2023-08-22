62d904eb25f59d22642c9c53f5799ce97bf49fd5
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-08-22 20:44:12 -0300

    php+html

---
+++ PHP/Get/form.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$ende = $_POST['ende'];
+
+echo "Olá $nome, tudo bem?<br>";
+
+echo "Seu email é $email<br>";
+echo "Sua senha é $senha<br>";
+echo "Seu endereço é $ende";
+
+?>
