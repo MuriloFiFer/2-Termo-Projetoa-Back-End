a7fcbe6fbe8abedec53a3004e8c3e3725ad57638
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-08-22 21:57:16 -0300

    PHP+HTML

---
+++ PHP/Funcao/form.php
@@ -0,0 +1,17 @@
+<?php
+
+include "funcao.php";
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$x = $_POST['num1'];
+$y = $_POST['num2'];
+
+echo"Soma=".Soma($x,$y)."<br>";
+echo"Subtração=".Sub($x,$y)."<br>";
+echo" Multiplicação=".Mult($x,$y)."<br>";
+echo" Potencia=".Mult($x,$y)."<br>";
+
+
+
+
+?>
---
+++ PHP/Funcao/funcao.php
@@ -0,0 +1,15 @@
+<?php
+	function Soma ($a,$b){
+		return ($a+$b);
+	}
+	function Sub ($a,$b){
+		return ($a-$b);
+	}
+	function Mult ($a,$b){
+		return ($a*$b);
+	}
+	function Potencia ($a,$b){
+		return ($a^$b);
+	}
+
+?>
